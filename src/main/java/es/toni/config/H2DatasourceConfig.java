1d0d87669958323b9a64a18a8d05251b3a3d6818
toni <ferreirocouto@gmail.com>
2020-09-29 12:41:44 +0100

    eliminacion aop springboot

--- src/main/java/es/toni/config/H2DatasourceConfig.java
+++ src/main/java/es/toni/config/H2DatasourceConfig.java
@@ -3,9 +3,14 @@ package es.toni.config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+@EnableTransactionManagement
 @Configuration
-public class H2DataSourceConfig {
+public class H2DataSourceConfig implements TransactionManagementConfigurer{
 
     @Bean
     public DataSource dataSource(){
@@ -16,4 +21,14 @@ public class H2DataSourceConfig {
         dataSource.setPassword("");
         return dataSource;
     }
+
+    @Bean
+    public PlatformTransactionManager txManager() {
+    	return new DataSourceTransactionManager(dataSource());
+    }
+    
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return txManager();
+	}
 }
